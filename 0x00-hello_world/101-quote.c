90cd61b18b7153c1e2c8f6ba13be8f3ad6cd04cc
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-07 10:13:00 -0400

    Printing message to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Prints some message
+ * Uses write function to print the message
+ * Return: 1 without failure.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
