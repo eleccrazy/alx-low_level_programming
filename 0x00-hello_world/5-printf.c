fa2797f34156b15413a379608ce7923d3642a425
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-07 09:35:04 -0400

    Printing message using printf() function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/*
+ * main - prints the message inside the printf function
+ *
+ * printf: is a function to print the message to the terminal
+ * return: returns 0
+ */
+
+
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
