e2f7936e3474d9e2abd7cdf3fea60e886c0c74c8
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-07 09:25:10 -0400

    This c code uses puts function to print the message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*  main - prints some message
+*
+*  Uses: Puts function to print the message
+*  Return: 0
+*/
+
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
