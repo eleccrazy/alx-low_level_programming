a848d17d2d8d583bf74c5f3424b568b483c5506a
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-08 10:46:29 -0400

    Fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -23,11 +23,8 @@ int main(void)
 						putchar(first + '0');
 						putchar(second + '0');
 						putchar(third + '0');
-						if (!(first == 7 && second == 8 && third == 9))
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						putchar(',');						
+						putchar(' ');
 					}
 				}
 			}
