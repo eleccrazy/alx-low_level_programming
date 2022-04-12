63c6c40136c3e887d40da221063a85053cf6b603
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-12 06:05:40 -0400

    Printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * main - prints some text with _putchar function
+  * Return: returns 0 with success
+  */
+
+int main(void)
+{
+	char *putchar;
+
+	for (putchar = "_putchar\n"; *putchar != '\0'; *putchar++)
+		_putchar(*putchar);
+
+	return (0);
+
+}
