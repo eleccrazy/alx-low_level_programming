7231baee4cb20b75870ed751777946472915adb0
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-12 06:13:57 -0400

    Fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char *putchar;
 
-	for (putchar = "_putchar\n"; *putchar != '\0'; *putchar++)
+	for (putchar = "_putchar\n"; *putchar != '\0'; putchar++)
 		_putchar(*putchar);
 
 	return (0);
