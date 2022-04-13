b0bec0ded0a9618219dd29b6c6e70d18fd157084
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-13 09:41:46 -0400

    fixed

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if ((a > b && b > c) || (a > c && c > b))
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if ((b > a && a > c) || (b > c && c > a))
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
