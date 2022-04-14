488b06a634b4e33b4e0786a4a020811cca65b560
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-14 07:54:30 -0400

    Print numbers from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * more_numbers - prints numbers from 0 - 14 10 times
+  */
+
+void more_numbers(void)
+{
+	int digits, lines;
+
+	for (lines = 0; lines < 10; lines++)
+	{
+		for (digits = 0; digits < 15; digits++)
+		{
+			if ( digits > 9)
+				_putchar((digits / 10) + '0');
+			_putchar((digits % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
