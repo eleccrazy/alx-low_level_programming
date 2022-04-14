21ecda9618fc04f96498c8e4582c7395d953e393
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-14 07:53:35 -0400

    Print digits except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - prints digits except 2 and 4
+  */
+
+void print_most_numbers(void)
+{
+	int numbers = 48;
+
+	while (numbers < 58)
+	{
+		if (numbers != 50 && numbers != 52)
+			_putchar(numbers);
+		numbers++;
+	}
+	_putchar('\n');
+}
