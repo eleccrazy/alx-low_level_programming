3aacf40e06cc3b6fca3fbc371b4b0752ba699ffe
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-14 07:55:56 -0400

    Drawing street lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_line: prints lines
+  * @n: an argument passed to print_line
+  */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+			_putchar('_');
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
+
