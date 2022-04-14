3532f623c0a6f49f8bde32a2fcc44a8565d0f3c3
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-14 08:06:00 -0400

    FizzBuzz is just fun

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * main - computes FizzBuzz
+  * Return: returns 0 with succes
+  */
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 1; numbers < 101; numbers++)
+	{
+		if ((numbers % 3 == 0) && (numbers % 5 != 0))
+			printf("Fizz");
+		else if ((numbers %  5 == 0) && (numbers % 3 != 0))
+			printf("Buzz");
+		else if ((numbers % 3 == 0) && (numbers % 5 == 0))
+			printf("FizzBuzz");
+		else
+			printf("%d", numbers);
+		printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
