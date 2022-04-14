70c04dc12c9e50c1cec54b3a77bc7957b2543449
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-14 07:52:35 -0400

    Printing digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints digits from 0 to 9
+  */
+
+void print_numbers(void)
+{
+	int numbers;
+
+	for (numbers = 48; numbers < 58; numbers++)
+		putchar(numbers);
+	putchar('\n');
+}
