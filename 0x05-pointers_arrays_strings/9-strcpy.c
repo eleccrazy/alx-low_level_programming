b5b0931341ef96db4f5dfa6f2e8d1fc178f5628a
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-18 12:56:48 -0400

    Copying strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+  * *_strcpy - copies string from source to destination
+  * @dest: an argument as destination of the string
+  * @src: an argument as source of the string
+  * Return: returns dest with success
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (*(src + count))
+	{
+		dest[count] = src[count];
+		count++;
+	}
+
+	return (dest);
+}
