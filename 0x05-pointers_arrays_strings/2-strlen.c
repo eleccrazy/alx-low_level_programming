071fdc2658c3998b93959bf3fe05b9c27c078951
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-18 08:27:29 -0400

    Calculating length of given string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _strlen - computes the length of the given string
+  * @s: a pointer point to character array
+  * Return: returns length of characters
+  */
+
+int _strlen(char *s)
+{
+	int length;
+	
+	length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+
+		s++;
+	}
+
+	return (length);
+}
