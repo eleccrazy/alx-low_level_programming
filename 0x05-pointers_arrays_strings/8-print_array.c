b27da6799684b66cbc922ba85105a02bf17ef0c1
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-18 11:08:09 -0400

    Fix betty code style

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,7 +14,7 @@ void print_array(int *a, int n)
 	for (index = 0; index < n; index++)
 	{
 		printf("%d", a[index]);
-		if (index != (n-1))
+		if (index != (n - 1))
 			printf(", ");
 	}
 	printf("\n");
