626f24740243124266b54babc21c81b4f749f1e0
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-20 03:01:41 -0700

    String Concatenation with byte limit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,21 @@
 #include "main.h"
 
+/**
+ * _strncat - concatenates two strings with limited in bytes
+ * @dest: a string argument to hold the concatenated string
+ * @src: a string argument to be concatenated
+ * @n: an argument that specifies how many bytes to be concatenated
+ * Return: returns dest
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int j = 0;
 
-	for(; dest[i] != '\0'; i++)
+	for (; dest[i] != '\0'; i++)
 		;
-	for(; src[j] != '\0'; j++)
+	for (; src[j] != '\0'; j++)
 	{
 		if (j < n)
 			dest[i + j] = src[j];
