db55ad7bc04629248db112c34cf8d3fd50797222
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-20 06:33:39 -0700

    Capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes the words
+ * @str: a string argument to be capitalized
+ * Return: a pointer to capitalized string
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' ||
+				str[i] == '\t' ||
+				str[i] == '\n' ||
+				str[i] == '.' ||
+				str[i] == 0)
+			if (str[i + 1] > 96 && str[i + 1] < 123)
+				str[i + 1] -= 32;
+	}
+
+	return (str);
+}
