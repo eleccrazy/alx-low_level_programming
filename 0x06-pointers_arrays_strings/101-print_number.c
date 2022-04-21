a14f83cfdd03a45f91280dc86e997194bcf1a111
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-20 22:58:12 -0700

    Print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number that it accepts
+ * @n: a number to be printed
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
