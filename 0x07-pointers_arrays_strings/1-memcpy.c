049abb17ae8724a6c99ca32072eb804b50a15e76
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-25 00:39:02 -0700

    Copy bytes to mem location

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies n bytes from memory area src to memory
+ * area dest.
+ *
+ * @dest: Memory location to hold the copied bytes
+ * @src: Memory area that holds bytes to be copied
+ * @n: Maximum number of bytes to be copied
+ *
+ * Return: Returns a pointer to memory location dest
+ * that holds the copied bytes.
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*dest = src[i];
+		i++;
+		dest++;
+	}
+
+	return (dest);
+}
