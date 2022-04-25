64149e3fae16ff455574a601a12a0cfc20f7ace9
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-25 00:17:13 -0700

    Header file

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+/**
+ * This header file contains all the prototypes required for the 
+ * 0x07-pointers_arrays_strings project.
+ * 
+ * @author: Gizachew Bayness
+ * 
+ * Date crated: April 25, 2022
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+
+#endif /* MAIN_H */
