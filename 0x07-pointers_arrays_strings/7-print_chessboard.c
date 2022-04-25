24e192a6d83b5c82564b7475d4c734627b069e86
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-25 02:23:15 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints the chessboard
+ * @a - A pointer that holds chessboard values to be printed
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j;
+
+	while (i < 8)
+	{
+		j = 0;
+		
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
