07281e83fc88ef262ded5eb088f289b082a4efec
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-27 01:02:57 -0700

    Update header files

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * This header file contains all the prototypes required for the
+ * 0x08-recursion project.
+ *
+ * @author: Gizachew Bayness
+ *
+ * Date crated: April 27, 2022
+ */
+
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
