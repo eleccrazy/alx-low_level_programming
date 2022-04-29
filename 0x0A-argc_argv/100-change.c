1e47c244391fce2810bbeb53cd7db0fb4e750605
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-29 05:05:25 -0700

    Fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,26 +1,58 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins to make change
- * for an amount of money
+ * coinConverter - Helper function that does all the mathematics
  *
- * @argc: number of command line arguments
- * @argv: name of the array to hold command line arguments
- *
- * Return:returns 1 if the arguments are not exactly 1, otherwise
- * returns 0 with success
+ * @i: Passed in variable from main for calculations
+ * 
+ * Return: The number of coins needed minimum for the passed in variable
+ */
+int coinConverter(int i)
+{
+	int count = 0;
+
+	while (i != 0)
+	{
+		if (i % 10 == 9 || i % 10 == 7)
+			i -= 2;
+		else if (i % 25 == 0)
+			i -= 25;
+		else if (i % 10 == 0)
+			i -= 10;
+		else if (i % 5 == 0)
+			i -= 5;
+			else if (i % 2 == 0)
+		{
+			if (i % 10 == 6)
+				i -= 1;
+			else
+				i -= 2;
+		}
+		else
+			i -= 1;
+
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * main - prints the minimum number of coins to make
+ * change for an amount of money
+ * 
+ * @argc: Number of command line arguments
+ * @argv: name of the array that holds the command line arugments.
+ * 
+ * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
  *
  */
-
 int main(int argc, char *argv[])
 {
-	int position, total, change, aux;
-	int coins[] = {25, 10, 5, 2, 1};
+	int i, coin;
 
-	position = total = change = aux = 0;
+	coin = 0;
 
 	if (argc != 2)
 	{
@@ -28,25 +60,17 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	total = atoi(argv[1]);
+	i = atoi(argv[1]);
 
-	if (total <= 0)
+	if (i < 0)
 		printf("0\n");
 
-	while (coins[position] != '\0')
-
+	else
 	{
-		if (total >= coins[position])
-		{
-			aux = (total / coins[position]);
-			change += aux;
-			total -= coins[position] * aux;
-		}
+		coin = coinConverter(i);
 
-		position++;
-
+		printf("%d\n", coin);
 	}
 
-	printf("%d\n", change);
 	return (0);
 }
