5dbec5b7655c1540127f2e9fab5c1b7d8503e04f
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-29 03:51:52 -0700

    Fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
 
 /**
  * main - adds positive numbers
@@ -11,6 +13,23 @@
  * otherwise it returns 0 with success
  *
  */
+
+int check_arg_for_digit(char *str)
+{
+	int i, len;
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		if (isdigit(str[i]) == 0)
+			return (0);
+		else
+			return (1);
+	}
+
+	return (0);
+}
 
 int main(int argc, char **argv)
 {
@@ -24,12 +43,11 @@ int main(int argc, char **argv)
 	{
 		for (; i < argc; i++)
 		{
-			if (*argv[i] < 48 || *argv[i] > 57)
+			if (check_arg_for_digit(argv[i]) == 0)
 			{
 				printf("Error\n");
 				return (1);
 			}
-
 			sum += atoi(argv[i]);
 		}
 
