30c2cf4b99b9f197a7cdea22789ec662cc00984b
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-29 05:02:07 -0700

    Fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<ctype.h>
 
 /**
  * main - prints the minimum number of coins to make change
