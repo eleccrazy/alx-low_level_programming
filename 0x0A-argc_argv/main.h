941eb753ec908c11ad203b1d515da32b77c77dc3
Gizachew Bayness <eleccrazy24@gmail.com>
2022-04-29 01:22:52 -0700

    Header file

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * This header file contains all the prototypes required for the
+ * 0x09-static_libraries project.
+ *
+ * @author: Gizachew Bayness
+ *
+ * Date crated: April 29, 2022
+ */
+
+int _putchar(char c);
+
+#endif
