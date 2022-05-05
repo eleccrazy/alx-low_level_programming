54d547b809b32b1111116e87b3f0bae021cc579c
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-05 00:49:00 -0700

    Grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,50 +2,81 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - This function computes and returns a pointer
- * to the two dimensional array of elements.
+ * init_grid - Initializes a 2-D grid array with the
+ * specified height and width value.
+ *
+ * @h: The height of the array to be initialized.
+ * @w: The width of the array to be initialized.
+ *
+ * Return: Returns NUL if it fails to create the array,
+ * otherwise it returns a pointer to the initialized 2-D array.
+ *
+ */
+
+int **init_grid(int h, int w)
+{
+	int **grid_init;
+	int index = 0;
+
+	grid_init = malloc(sizeof(int *) * h);
+
+	if (grid_init == NULL)
+		return (NULL);
+
+	while (index < h)
+	{
+		grid_init[index] = malloc(sizeof(int) * w);
+
+		if (grid_init[index] == NULL)
+		{
+			index -= 1;
+			while (index >= 0)
+			{
+				free(grid_init[index]);
+				index--;
+			}
+
+			free(grid_init);
+		}
+		index++;
+	}
+
+	return (grid_init);
+}
+
+/**
+ * alloc_grid - This stores a value on a 2-D grid array and
+ * returns a pointer to the two dimensional array of elements.
  *
  * @width: number of colomons of the 2-D array
  * @height: number of rows of the 2-D array
  *
- * Return: returns NULL if width or height of the 2-D array is
- * negartive / 0 or if it fails to create a memory location,
- * otherwise it returns a pointer to a 2-D array of integers.
+ * Return: returns NULL if width or height of the 2-D array is less
+ * than 1, otherwise it returns a pointer to a 2-D array of integers.
  *
  */
 
 int **alloc_grid(int width, int height)
 {
-	int **twoD_array;
+	int **grid;
 	int i, j;
 
-	if (width <= 0 || height <= 0)
+	if (height < 1 || width < 1)
 		return (NULL);
 
-	twoD_array = malloc(sizeof(int *) * height);
+	grid = init_grid(height, width);
 
-	if (twoD_array == NULL)
-		return (NULL);
-
-	for (i = 0; i < height; i++)
+	i = 0;
+	while (i < height)
 	{
-		twoD_array[i] = malloc(sizeof(int) * width);
-
-		if (twoD_array[i] == NULL)
+		j = 0;
+		while (j < width)
 		{
-			for (i -= 1; i >= 0; i--)
-				free(twoD_array[i]);
-
-			free(twoD_array);
-			return (NULL);
+			grid[i][j] = 0;
+			j++;
 		}
+		i++;
 	}
 
-
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-			twoD_array[i][j] = 0;
-	}
-	return (twoD_array);
+	return (grid);
 }
