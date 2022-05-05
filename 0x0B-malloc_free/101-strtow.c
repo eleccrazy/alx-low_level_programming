842ff72f95aa3bb030dc4ddbb5b990f91b2fec65
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-05 06:14:02 -0700

    String to Word Splitted

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -47,15 +47,58 @@ int count_words(char *str)
 	return (word_count);
 }
 
+/**
+ * init_str - Initializes a 2-D array to hold words splitted from
+ * a given string.
+ *
+ * @str: A string whose words to be splitted.
+ *
+ * Return: returns NULL if the number of words in a given string are
+ * 0 or if it fails to create a memory space, otherwise it returns
+ * a pointer to an array of strings.
+ *
+ */
+
+char **init_str(char *str)
+{
+	char **new_str;
+	int i = 0, no_of_words, no_of_letters, index = 0;
+
+	no_of_words = count_words(str);
+	if (no_of_words == 0)
+		return (NULL);
+
+	new_str = malloc(sizeof(char *) * no_of_words + 1);
+
+	if (new_str == NULL)
+		return (NULL);
+	for (; i < no_of_words; i++)
+	{
+		while (str[index] == ' ')
+			index++;
+
+		no_of_letters = count_word_len(str + index);
+		new_str[i] = malloc(sizeof(char) * no_of_letters + 1);
+
+		if (new_str[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(new_str[i]);
+			free(new_str);
+			return (NULL);
+		}
+	}
+
+	return (new_str);
+}
 
 /**
  * strtow - Splits a string into words.
  *
  * @str: The string to be splitted.
  *
- * Return: Returns NULL if str is null or str quals empty
- * string or if it fails to create a memory, otherwise it
- * returns a pointer to an array of strings (words).
+ * Return: Returns NULL if str is null or str quals empty string,
+ * otherwise it returns a pointer to an array of strings (words).
  *
  */
 
@@ -68,10 +111,9 @@ char **strtow(char *str)
 		return (NULL);
 
 	no_of_words = count_words(str);
-	if (no_of_words == 0)
-		return (NULL);
 
-	word_from_str = malloc(sizeof(char *) * (no_of_words + 1));
+	word_from_str = init_str(str);
+
 	if (word_from_str == NULL)
 		return (NULL);
 
@@ -80,23 +122,14 @@ char **strtow(char *str)
 		while (str[i] == ' ')
 			i++;
 		word_len = count_word_len(str + i);
-		word_from_str[w_index] = malloc(sizeof(char) * (word_len + 1));
-
-		if (word_from_str[w_index] == NULL)
-		{
-			for (; w_index >= 0; w_index--)
-				free(word_from_str[w_index]);
-
-			free(word_from_str);
-			return (NULL);
-		}
 
 		for (l_index = 0; l_index < word_len; l_index++)
 			word_from_str[w_index][l_index] = str[i++];
 
-		word_from_str[w_index][l_index] = '\0';
+		word_from_str[w_index][word_len] = '\0';
 	}
-	word_from_str[w_index] = NULL;
+
+	word_from_str[no_of_words] = NULL;
 
 	return (word_from_str);
 }
