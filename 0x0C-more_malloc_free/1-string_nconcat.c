aeb0e0c28a068182dc116d733208049518e9c66e
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-06 04:13:27 -0700

    Bug Fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,28 +2,6 @@
 #include <stdlib.h>
 
 /**
- * length_of_string - calculates the length of the string.
- *
- * @str: The string whose length to be computed.
- *
- * Return: returns the length of the string.
- *
- */
-
-int length_of_string(char *str)
-{
-	int len, index = 0;
-
-	while (str[index])
-	{
-		len++;
-		index++;
-	}
-
-	return (len);
-}
-
-/**
  * string_nconcat - Concatenates two strings with first n bytes
  * of string s2 to s1.
  *
@@ -40,28 +18,29 @@ int length_of_string(char *str)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *new_str;
-	unsigned int len, index = 0;
+	unsigned int len = n, i = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	len = length_of_string(s1);
-	len += n;
+	len += sizeof(s2);
 
 	new_str = malloc(sizeof(char) * len);
 
 	if (new_str == NULL)
 		return (NULL);
+	len = 0;
+	i = 0;
 
-	for (len = 0; s1[index]; len++, index++)
-		new_str[len] = s1[index];
+	for (; s1[i]; len++, i++)
+		new_str[len] = s1[i];
 
-	for (index = 0; s2[index] && index < n; index++, len++)
-		new_str[len] = s2[index];
+	for (i = 0; s2[i] && i < n; i++)
+		new_str[len++] = s2[i];
 
-	new_str[len] = '\0';
+	new_str[len++] = '\0';
 
 	return (new_str);
 
