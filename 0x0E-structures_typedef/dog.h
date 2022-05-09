ee9cb78ccacf70e75efa31c2668c346135915ecf
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-09 07:01:06 -0700

    Update header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -30,6 +30,12 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for dog struct.
+ */
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
