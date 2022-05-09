fdeb20492807f68ca0a26481cf7aff858c43843d
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-09 00:26:41 -0700

    Function like macro for adding two numbers

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,21 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 0x0D-preprocessor
+ *
+ * Desc: This heder file defines a function like macro named
+ * SUM(x, y) that computes the sum of the numbers x and y.
+ *
+ * @x: The first number to be added.
+ * @y: The second number to be added.
+ *
+ * Author: Gizachew Bayness (eleccrazy)
+ *
+ * Date Created: May 9, 2022
+ *
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
