97ee6d95b60514f236a67328d922de2b79dd5f25
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-08 23:46:04 -0700

    Defining a macro for PI

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,15 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * Desc: This header file defines a macro named PI as an
+ * abbreviation for the token 3.14159265359.
+ *
+ * @author: Gizachew Bayness (eleccrazy)
+ *
+ * Date Created: May 8, 2022
+ */
+
+#define PI 3.14159265359
+
+#endif
