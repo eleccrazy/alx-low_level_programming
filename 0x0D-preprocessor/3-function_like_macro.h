8f20d0f68e39655a9fdd88430d940538b2f87f49
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-09 00:02:41 -0700

    Computing absolute value of a number with Macros

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,19 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * File: 3-function_like_macro.h
+ * Desc: Heder file that defines a function like macro named
+ * ABS(x) that computes the absolute value of a number x.
+ *
+ * @x: The number whose absolute value is going to be computed.
+ *
+ * Author: Gizachew Bayness (eleccrazy).
+ *
+ * Date Created: May 8, 2022
+ *
+ */
+
+#define ABS(x) ((x >= 0) ? (x) : (-x))
+
+#endif
