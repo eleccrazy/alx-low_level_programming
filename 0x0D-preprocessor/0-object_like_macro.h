321aaa34ec5e775ac7ab05f976fa9e5efb435839
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-09 00:06:38 -0700

    Betty fixed

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,10 +2,11 @@
 #define OBJECT_LIKE_MACRO_H
 
 /**
+ * File: 0-object_like_macro.h
  * Desc: This header file defines a macro named SIZE an
  * abbreviation for token 1024.
  *
- * @author: Gizachew Bayness (eleccrazy).
+ * Author: Gizachew Bayness (eleccrazy).
  *
  * Date Created: May 8, 2022
  */
