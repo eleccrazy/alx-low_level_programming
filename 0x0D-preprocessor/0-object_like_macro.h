9689b184e93df877337db26abcaa4a2ab1f5b07e
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-08 23:40:59 -0700

    Defining object like macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,15 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * Desc: This header file defines a macro named SIZE an
+ * abbreviation for token 1024.
+ *
+ * @author: Gizachew Bayness (eleccrazy).
+ *
+ * Date Created: May 8, 2022
+ */
+
+#define SIZE 1024
+
+#endif
