0be90aa22a226e70e2d93cbcdf0592b75821d6b0
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-10 23:27:10 -0700

    Print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - passes name to the function pointed by f.
+ *
+ * @name: The name to be printed.
+ * @f: A function pointer that points to a function, which
+ * prints the name.
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
