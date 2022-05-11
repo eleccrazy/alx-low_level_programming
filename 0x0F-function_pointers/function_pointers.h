c4b03182987d82abbb5945551d85f40f139228f5
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-10 23:17:38 -0700

    Header file

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,20 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/*
+ * File: function_pointers.h
+ *
+ * Desc: This header file contains the prototypes used in
+ * the 0x0F-function_pointers project.
+ *
+ * Author: Gizachew Bayness (eleccrazy)
+ *
+ * Date Created: May 10, 2022
+ *
+ */
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
