d094ee115107c1e4f379cb73d14656868f26e33f
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-12 07:25:05 -0700

    Update Header File

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -18,4 +18,19 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct my_str - My structure
+ *
+ * @format: pointer to format which is to be printed.
+ * @printer: function pointer to select function.
+ *
+ */
+
+typedef struct my_str
+{
+	char *fmt;
+	void (*printer)();
+
+} print_struct;
+
 #endif
