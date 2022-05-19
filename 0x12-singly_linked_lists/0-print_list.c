582a416079b8174963fcbf9cca2030eaddd9988b
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-19 03:49:44 -0700

    Betty Fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,7 +19,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n",h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		count_nodes++;
 		h = h->next;
 	}
