8a643ce537b49a54a33c382685493f8263c6509e
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-19 05:12:21 -0700

    Betty fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,5 +8,6 @@
 
 void __attribute__ ((constructor)) print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
