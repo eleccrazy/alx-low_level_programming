d2f0875f6dd0d2c54e702ced4f9621af7d20c82b
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-23 08:27:00 -0700

    Delete list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - Delets the entire linked list
+ *
+ * @head: A pointer to the head of the list
+ *
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		free(head);
+
+		head = temp->next;
+	}
+}
