8514c21710e320a7be9bdef467476aa0081b1f5a
Gizachew Bayness <eleccrazy24@gmail.com>
2022-05-23 10:01:25 -0700

    Bug Fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,10 +18,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *traverse, *prev_node;
 
-	if (head == NULL || *head == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL || *head = NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
 	new_node->n = n;
 	new_node->next = NULL;
 
