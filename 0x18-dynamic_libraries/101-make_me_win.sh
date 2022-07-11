f1000649494eaef89e9a1eed7ed013574c46663a
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-11 07:55:10 -0700

    Fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/eleccrazy/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libeleccrazy.so
-export LD_PRELOAD=/tmp/libeleccrazy.so
+wget -P .. /tmp/ https://raw.githubusercontent.com/eleccrazy/alx-low_level_programming/master/0x18-dynamic_libraries/libeleccrazy.so
+export LD_PRELOAD=/tmp/../libeleccrazy.so
