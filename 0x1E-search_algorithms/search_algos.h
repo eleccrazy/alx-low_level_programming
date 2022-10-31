ca00263117d2dcac7da12948b0eea136ac9c3999
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-31 13:52:37 +0300

    header files

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,19 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/*
+ * File: search_algos.h
+ * Desc: This header file contains all the prototypes required for
+ * the 0x1E-search_algorithms project.
+ *
+ * Author: Gizachew Bayness (Elec Crazy)
+ * Date Created: Oct 31, 2022
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+
+
+#endif
