af1af0600c3977969936b268472c4a0da3d41704
Gizachew Bayness <eleccrazy24@gmail.com>
2022-11-02 22:29:05 +0300

    header file updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,6 +10,7 @@
  * Date Created: Oct 31, 2022
  *
  */
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -19,6 +20,6 @@ int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
-
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
